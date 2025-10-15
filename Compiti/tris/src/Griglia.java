f65b334f88b83767f8997ee277b42cd597aa2d55
il-leoleo <panzaleo08@gmail.com>
2025-10-15 21:15:41 +0200

    Finito il TRIS

--- Compiti/tris/src/Griglia.java
+++ Compiti/tris/src/Griglia.java
@@ -2,11 +2,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Creates a grid to play tris
+ * @Author: Panza Leonardo
+ * @Version: 1.0
+ */
 public class Griglia {
 
     private final int LATO_GRIGLIA = 3;
     private char[][] griglia = new char[LATO_GRIGLIA][LATO_GRIGLIA];
 
+
+    /**
+     * Initializes the grid
+     */
     public Griglia(){
 
         //dichiaro tutti gli spazi vuoti
@@ -18,36 +28,36 @@ public class Griglia {
 
     }
 
-    private List convertiCoord(int num){
-        List a = new ArrayList<Integer>();
 
-        if(num == 1 || num == 4 || num == 7) a.add(0);
-        if(num == 2 || num == 5 || num == 8) a.add(1);
-        if(num == 3 || num == 6 || num == 9) a.add(2);
-        if(num == 7 || num == 8 || num == 9) a.add(0);
-        if(num == 4 || num == 5 || num == 6) a.add(1);
-        if(num == 1 || num == 2 || num == 3) a.add(2);
-
-        if(griglia[(int) a.get(0)][(int) a.get(1)] != ' ') throw new IllegalArgumentException("Cella già occupata");
-        IO.println(a);
-        return a;
-    }
-
+    /**
+     * Adds the X symbol in the selected slot
+     * @param coordinate the number of the selected slot, it must be a integer and between 1 and 9
+     */
     public void aggiungiX(int coordinate){
 
-        List a = convertiCoord(coordinate);
+        List<Integer> a = convertiCoord(coordinate);
 
-        griglia[(int) a.get(1)][(int) a.get(0)] = 'X';
+        griglia[a.get(0)][a.get(1)] = 'X';
 
     }
 
+    /**
+     * Adds the O symbol in the selected slot
+     * @param coordinate the number of the selected slot, it must be a integer and between 1 and 9
+     */
     public void aggiungiO(int coordinate){
 
-        List a = convertiCoord(coordinate);
+        List<Integer> a = convertiCoord(coordinate);
 
-        griglia[(int) a.get(0)][(int) a.get(1)] = 'O';
+        griglia[a.get(0)][a.get(1)] = 'O';
 
     }
+
+
+    /**
+     * Graphically prints the current state of the grid in the terminal,
+     * using characters to represent each cell.
+     */
 
     public void mostraGriglia(){
         for(int i = 0; i<LATO_GRIGLIA; i++){
@@ -62,5 +72,93 @@ public class Griglia {
         }
     }
 
+
+    /**
+     * Checks the winner of the game.
+     *
+     * @return 'X' if the player with symbol X has won,
+     *         'O' if the player with symbol O has won,
+     *         ' ' (space) if there is no winner yet.
+     */
+
+    public char check(){
+
+        List<Character> a  = new ArrayList<>();
+
+        for (int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                a.add(griglia[i][j]);
+            }
+        }
+
+        char vincitore = ' ';
+
+        //Controllo se c'è un tris orizzontale
+        for (int i = 0; i<9; i = i+3) {
+            char primo = a.get(i);
+            if (primo == a.get(i+1) && primo == a.get(i+2)) {
+                if(primo == 'X'){
+                    vincitore = 'X';
+                }else if(primo == 'O'){
+                    vincitore = 'O';
+                }
+            }
+        }
+
+        //Controllo se c'è un tris verticale
+        if(vincitore == ' ') {
+            for (int i = 0; i < 3; i++) {
+                char primo = a.get(i);
+                if (primo == a.get(i + 3) && primo == a.get(i + 6)) {
+                    if (primo == 'X') {
+                        vincitore = 'X';
+                    } else if(primo == 'O'){
+                        vincitore = 'O';
+                    }
+                }
+            }
+        }
+
+        //Controllo la diagonale basso sx - alto dx
+        if(vincitore == ' '){
+            if(a.get(0) == a.get(4) && a.get(0) == a.get(8)){
+                if(a.get(0)=='X'){
+                    vincitore = 'X';
+                }else if(a.get(0) == 'O'){
+                    vincitore = 'O';
+                }
+            }
+        }
+
+        //Controllo la diagonale basso dx - alto sx
+        if(vincitore == ' '){
+            if(a.get(2) == a.get(4) && a.get(2) == a.get(6)){
+                if(a.get(2)=='X'){
+                    vincitore = 'X';
+                }else if(a.get(2) == 'O'){
+                    vincitore = 'O';
+                }
+            }
+        }
+
+        return vincitore;
+
+    }
+
+    private List convertiCoord(int num){
+        List<Integer> a = new ArrayList<Integer>();
+
+        if(num == 7 || num == 8 || num == 9) a.add(0);
+        if(num == 4 || num == 5 || num == 6) a.add(1);
+        if(num == 1 || num == 2 || num == 3) a.add(2);
+        if(num == 1 || num == 4 || num == 7) a.add(0);
+        if(num == 2 || num == 5 || num == 8) a.add(1);
+        if(num == 3 || num == 6 || num == 9) a.add(2);
+
+
+        if(griglia[a.get(0)][a.get(1)] != ' ') throw new IllegalArgumentException("Cella già occupata");
+
+        return a;
+    }
 
 }
--- Compiti/tris/src/Main.java
+++ Compiti/tris/src/Main.java
@@ -3,15 +3,119 @@
 
 void main() {
 
-    boolean gayming = true;
-    Griglia a = new Griglia();
 
-    while(gayming) {
+    //Stampo le istruzioni
+    IO.println("\n----- Benvenuto/a al gioco del TRIS -----\n" +
+            "Si giocherà a turni, prima giocatore 1 (X), poi giocatore 2 (O)\n" +
+            "Per inserire il tuo simbolo, utilizza il tastierino numerico come se fosse la tabella di tris\n" +
+            "In questo modo: \n" +
+            "7 | 8 | 9 |\n" +
+            "----------|\n" +
+            "4 | 5 | 6 |\n" +
+            "----------|\n" +
+            "1 | 2 | 3 |\n" +
+            "Dopo aver inserito la casella desiderata, premere INVIO per confermare la scelta\n" +
+            "\n----- PREMI INVIO PER COMINCIARE ----- \n");
+
+    IO.readln();
+    int turni = 0;
+    boolean gayming = true;
+    boolean err = true;
+    int num = 0;
+    char vinci = ' ';
+    Griglia a = new Griglia();
+    a.mostraGriglia();
+
+    while (gayming) {
+
+        turni++;
+        err = true;
+        while (err) {
+            err = false;
+            IO.print("Giocatore 1 (X): ");
+            String in = IO.readln();
+            try {
+                num = Integer.parseInt(in);
+                if (num < 1 || num > 9) {
+                    IO.println("Imposta un numero da 1 a 9");
+                    err = true;
+                }
+            } catch (Exception e) {
+                IO.println("Devi inserire un numero");
+                err = true;
+            }
+
+            //Se va tutto bene provo a mettere la X
+            if(!err) {
+                try {
+                    a.aggiungiX(num);
+                } catch (Exception e) {
+                    IO.println("Cella già occupata, scegli una cella libera!");
+                    err = true;
+                }
+            }
+        }
+
+        //Dopo i controlli inserisco la X e mostro la griglia
         a.mostraGriglia();
-        IO.print("Input: ");
-        String b = IO.readln();
-        int num = Integer.parseInt(b);
-        a.aggiungiX(num);
-        a.mostraGriglia();
+
+        //Controllo se qualcuno ha vinto se ho fatto più di 2 turni
+        if(turni>2) {
+            vinci = a.check();
+            if (vinci == 'X') {
+                IO.println("\nVince la X!");
+                gayming = false;
+            } else if (vinci == 'O') {
+                IO.println("\nVince la O!");
+                gayming = false;
+            }
+        }
+
+        //Controllo se X ha vinto durante la sua giocata
+        if(gayming){
+            err = true;
+            while (err) {
+                err = false;
+                IO.print("Giocatore 2 (O): ");
+                String in = IO.readln();
+                try {
+                    num = Integer.parseInt(in);
+                    if (num < 1 || num > 9) {
+                        IO.println("Imposta un numero da 1 a 9");
+                        err = true;
+                    }
+                } catch (Exception e) {
+                    IO.println("Devi inserire un numero");
+                    err = true;
+                }
+
+                //Se va tutto bene provo a mettere la X
+                if(!err) {
+                    try {
+                        a.aggiungiO(num);
+                    } catch (Exception e) {
+                        IO.println("Cella già occupata, scegli una cella libera!");
+                        err = true;
+                    }
+                }
+            }
+
+            a.mostraGriglia();
+
+            if(turni>2) {
+                vinci = a.check();
+                if (vinci == 'X') {
+                    IO.println("\nVince la X!");
+                    gayming = false;
+                } else if (vinci == 'O') {
+                    IO.println("\nVince la O!");
+                    gayming = false;
+                }
+            }
+        }
+        if(turni>=9){
+            gayming = false;
+            IO.println("Pareggio!");
+        }
     }
 }
